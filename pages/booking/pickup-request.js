faab80a16ae980db40d22b40a577ae7832d400f3
Muhammad Ismail <ismail.muhammad@tcs.com.pk>
2021-10-18 11:51:27 +0500

    ismail

--- pages/booking/pickup-request.js
+++ pages/booking/pickup-request.js
@@ -36,19 +36,6 @@ const PickupRequest = () => {
           <Card heading="Customer Information">
             <div className="flex flex-col items-center gap-3">
               <div className="flex-1 flex items-center gap-4 w-full">
-                <label className="label">Consignment Note #</label>
-                <div className="flex-1 flex items-center justify-center">
-                  <input
-                    type="text"
-                    className="focus:outline-none text-sm flex-1 p-2 rounded-l-md"
-                    {...register("consignment")}
-                  />
-                  <div className="bg-[#2F3C4B] w-[3rem] h-[2.2rem] flex justify-center items-center rounded-r-md">
-                    <img src="/icons/search2.svg" alt="" className="h-4" />
-                  </div>
-                </div>
-              </div>
-              <div className="flex-1 flex items-center gap-4 w-full">
                 <label className="label">
                   Cost Center <span className="text-[#FF0000]">*</span>
                 </label>
@@ -61,124 +48,50 @@ const PickupRequest = () => {
                 </select>
               </div>
               <div className="flex-1 flex items-center gap-4 w-full">
-                <div className="flex-1 flex items-center gap-4 w-full">
-                  <label className="label">Order Ref</label>
-                  <input
-                    type="text"
-                    className="input"
-                    {...register("orderRef")}
-                  />
-                </div>
-                <div className="flex-1 flex items-center gap-4 w-full">
-                  <label className="label">
-                    Contact <span className="text-[#FF0000]">*</span>
-                  </label>
-                  <div className="flex flex-col">
-                    <input
-                      type="text"
-                      className="input"
-                      {...register("contact", { required: true })}
-                    />
-                    {errors.contact && (
-                      <span className="requiredField">
-                        This field is required
-                      </span>
-                    )}
-                  </div>
-                </div>
+                <label className="label">Customer Name</label>
+                <input
+                  type="text"
+                  className="input2 text-[#464E5F] text-sm"
+                  {...register("address")}
+                  disabled
+                />
               </div>
               <div className="flex-1 flex items-center gap-4 w-full">
-                <div className="flex-1 flex items-center gap-4 w-full">
-                  <label className="label">
-                    Name <span className="text-[#FF0000]">*</span>
-                  </label>
-                  <div className="flex flex-col">
-                    <input
-                      type="text"
-                      className="input"
-                      {...register("name", { required: true })}
-                    />
-                    {errors.name && (
-                      <span className="requiredField">
-                        This field is required
-                      </span>
-                    )}
-                  </div>
-                </div>
-                <div className="flex-1 flex items-center gap-4 w-full">
-                  <label className="label">
-                    Delivery Type <span className="text-[#FF0000]">*</span>
-                  </label>
-                  <select
-                    type="text"
-                    className="input text-[#464E5F] text-sm"
-                    {...register("deliveryType", { required: true })}
-                  >
-                    <option value="Please Select">Please Select</option>
-                  </select>
-                </div>
+                <label className="label">Contact Number</label>
+                <input
+                  type="text"
+                  className="input2 text-[#464E5F] text-sm"
+                  {...register("address")}
+                  disabled
+                />
               </div>
               <div className="flex-1 flex items-center gap-4 w-full">
-                <div className="flex-1 flex items-center gap-4 w-full">
-                  <label className="label">
-                    City <span className="text-[#FF0000]">*</span>
-                  </label>
-                  <select
-                    type="text"
-                    className="input text-[#464E5F] text-sm"
-                    {...register("city", { required: true })}
-                  >
-                    <option value="Please Select">Please Select</option>
-                  </select>
-                </div>
-                <div className="flex-1 flex items-center gap-4 w-full">
-                  <label className="label">
-                    Email <span className="text-[#FF0000]">*</span>
-                  </label>
-                  <div className="flex flex-col">
-                    <input
-                      type="email"
-                      className="input"
-                      {...register("email", { required: true })}
-                    />
-                    {errors.email && (
-                      <span className="requiredField">
-                        This field is required
-                      </span>
-                    )}
-                  </div>
-                </div>
+                <label className="label">Email Address</label>
+                <input
+                  type="email"
+                  className="input2 text-[#464E5F] text-sm"
+                  {...register("address")}
+                  disabled
+                />
               </div>
               <div className="flex-1 flex items-center gap-4 w-full">
-                <div className="flex-1 flex items-center gap-4 w-full">
-                  <label className="label">
-                    Expr Center <span className="text-[#FF0000]">*</span>
-                  </label>
-                  <select
-                    type="text"
-                    className="input text-[#464E5F] text-sm"
-                    {...register("exprCenter", { required: true })}
-                  >
-                    <option value="Please Select">Please Select</option>
-                  </select>
-                </div>
-                <div className="flex-1 flex items-center gap-4 w-full">
-                  <label className="label">
-                    Address <span className="text-[#FF0000]">*</span>
-                  </label>
-                  <div className="flex flex-col">
-                    <input
-                      type="text"
-                      className="input"
-                      {...register("address", { required: true })}
-                    />
-                    {errors.address && (
-                      <span className="requiredField">
-                        This field is required
-                      </span>
-                    )}
-                  </div>
-                </div>
+                <label className="label">Contact Person</label>
+                <input
+                  type="email"
+                  className="input2 text-[#464E5F] text-sm"
+                  {...register("address")}
+                  disabled
+                />
+              </div>
+              <div className="flex-1 flex items-center gap-4 w-full">
+                <label className="label">Address</label>
+                <input
+                  type="email"
+                  className="input2 text-[#464E5F] text-sm"
+                  {...register("address")}
+                  value=""
+                  disabled
+                />
               </div>
             </div>
           </Card>
@@ -187,27 +100,39 @@ const PickupRequest = () => {
           <Card heading="PickUp Call Details">
             <div className="flex flex-col items-center gap-3">
               <div className="flex-1 flex items-center gap-4 w-full">
-                <label className="label">Customer Name</label>
-                <input
-                  type="name"
-                  className="input2"
-                  {...register("customerName")}
-                />
+                <label className="label">
+                  Shipment Type <span className="text-[#FF0000]">*</span>
+                </label>
+                <select
+                  type="text"
+                  className="input2 text-[#464E5F] text-sm"
+                  {...register("costCenter", { required: true })}
+                  disabled
+                >
+                  <option value="Please Select">Please Select</option>
+                </select>
               </div>
               <div className="flex-1 flex items-center gap-4 w-full">
-                <label className="label">Customer Number</label>
-                <input
-                  type="number"
-                  className="input2"
-                  {...register("customerNumber")}
-                />
+                <label className="label">
+                  Pickup Time <span className="text-[#FF0000]">*</span>
+                </label>
+                <select
+                  type="text"
+                  className="input text-[#464E5F] text-sm"
+                  {...register("costCenter", { required: true })}
+                >
+                  <option value="Please Select">Please Select</option>
+                </select>
               </div>
               <div className="flex-1 flex items-center gap-4 w-full">
-                <label className="label">Email</label>
+                <label className="label">
+                  Mobile # <span className="text-[#FF0000]">*</span>
+                </label>
                 <input
-                  type="email"
-                  className="input2"
+                  type="text"
+                  className="input"
                   {...register("customerEmail")}
+                  pattern="^((\+92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$"
                 />
               </div>
               <div className="flex-1 flex items-center gap-4 w-full">
@@ -230,7 +155,7 @@ const PickupRequest = () => {
           </Card>
           {/* Customer End */}
         </div>
-       
+
         <div className="media mx-auto p-4 flex justify-center items-center gap-2">
           <Button
             text="Refresh"
