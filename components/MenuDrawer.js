b63865a7931b4296a04a7defd261af26c7c61423
Muhammad Ismail <ismail.muhammad@tcs.com.pk>
2021-10-28 12:47:36 +0500

    ismail

--- components/MenuDrawer.js
+++ components/MenuDrawer.js
@@ -1,12 +1,30 @@
 import { Drawer, Button, Radio, Space } from "antd";
 import { useState } from "react";
-import DashboardIcon from '@mui/icons-material/Dashboard';
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ListSubheader from "@mui/material/ListSubheader";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import SendIcon from "@mui/icons-material/Send";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import StarBorder from "@mui/icons-material/StarBorder";
+import AssessmentIcon from "@mui/icons-material/Assessment";
 
 const App = ({ visible, setVisible }) => {
   const [placement, setPlacement] = useState("left");
+  const [open, setOpen] = useState(true);
 
   const onClose = () => {
     setVisible(false);
+  };
+
+  const handleClick = () => {
+    setOpen(!open);
   };
 
   return (
@@ -20,12 +38,68 @@ const App = ({ visible, setVisible }) => {
         key={placement}
         style={{ padding: 0 }}
       >
-        <ul className="mt-2">
-          <li className="flex gap-[1.5rem] items-center bg-[#F3F6F9] hover:bg-[#F3F6F9] py-4 px-6"><DashboardIcon/> Dashboard</li>
-          <li className="flex gap-[1.5rem] items-center hover:bg-[#F3F6F9] py-4 px-6"><DashboardIcon/> Bookings</li>
-          <li className="flex gap-[1.5rem] items-center hover:bg-[#F3F6F9] py-4 px-6"><DashboardIcon/> Reports</li>
-          <li className="flex gap-[1.5rem] items-center hover:bg-[#F3F6F9] py-4 px-6"><DashboardIcon/> Logout</li>
-        </ul>
+        <div className="flex flex-col t h-full">
+          <img src="/logo.svg" alt="" className="h-8 m-6 self-start" />
+          <div className="flex-1">
+            <List
+              sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+              component="nav"
+              aria-labelledby="nested-list-subheader"
+            >
+              <ListItemButton>
+                <ListItemIcon>
+                  <img src="/drawer/dashboard.svg" alt="" className="" />
+                </ListItemIcon>
+                <ListItemText className="text-sm" primary="Dashboard" />
+              </ListItemButton>
+              <ListItemButton>
+                <ListItemIcon>
+                  <img src="/drawer/reports.svg" alt="" className="" />
+                </ListItemIcon>
+                <ListItemText primary="Reports" />
+              </ListItemButton>
+              <ListItemButton>
+                <ListItemIcon>
+                  <img src="/drawer/setup.svg" alt="" className="" />
+                </ListItemIcon>
+                <ListItemText primary="Setup" />
+              </ListItemButton>
+              <ListItemButton onClick={handleClick}>
+                <ListItemIcon>
+                  <img src="/drawer/account.svg" alt="" className="" />
+                </ListItemIcon>
+                <ListItemText primary="My Account" />
+                {open ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>{/* <StarBorder /> */}</ListItemIcon>
+                    <ListItemText primary="Profile" />
+                  </ListItemButton>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>{/* <StarBorder /> */}</ListItemIcon>
+                    <ListItemText primary="Services" />
+                  </ListItemButton>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>{/* <StarBorder /> */}</ListItemIcon>
+                    <ListItemText primary="Manage Users" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+            </List>
+          </div>
+          <div className="drawer-logout">
+            <List>
+              <ListItemButton>
+                <ListItemIcon>
+                  <img src="/drawer/logout.svg" alt="" className="" />
+                </ListItemIcon>
+                <ListItemText primary="Logout" />
+              </ListItemButton>
+            </List>
+          </div>
+        </div>
       </Drawer>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,30 @@
-import { ConstructionOutlined } from "@mui/icons-material";
-import { Button } from "antd";
 import { useEffect, useRef, useState } from "react";
 import login from "../api/login";
 import { useRouter } from "next/router";
 import { parseCookies } from "../helpers/";
 import { useCookies } from "react-cookie";
+import { useForm } from "react-hook-form";
+import { CircularProgress } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
   const router = useRouter();
-  const username = useRef();
-  const password = useRef();
+  // const username = useRef();
+  // const password = useRef();
   const [token, setToken] = useState(null);
   const [cookie, setCookie] = useCookies(["token"]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [incorrect, setIncorrect] = useState(false);
 
-  const registeruser = async (event) => {
-    event.preventDefault();
-    const res = await login(username.current.value, password.current.value);
+  const handleLogin = async (username, password) => {
+    setIsLoading(true);
+    const res = await login(username, password);
     console.log(res);
     setToken(res);
     if ("token" in res) {
@@ -26,39 +35,95 @@ const Login = () => {
         sameSite: true,
       });
       router.push("/dashboard");
+    } else {
+      setIsLoading(false);
+      setIncorrect(true);
     }
   };
 
+  const onSubmit = async (data) => {
+    handleLogin(data.username, data.password);
+    console.log(data.username);
+  };
   return (
-    <form className="flex justify-center items-center w-full h-[100vh]">
-      <div className="flex-1 items-center flex flex-col w-full h-full">
-        <img src="/logo.svg" alt="" className="h-12 mt-12" />
-        <div className="flex-1 flex flex-col justify-center items-center mt-[-3rem]">
-          <h1 className="font-semibold text-[2rem]">Sign in</h1>
-          <div className="flex flex-col mt-2">
-            <label htmlFor="" className="font-semibold">
-              Email
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col w-full h-[100vh]"
+      style={{
+        backgroundImage: "url(/login-image.jpg)",
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="flex-1 flex justify-end items-center w-full">
+        <div className="bg-[#F1F4F6] w-[25rem] rounded-md p-[2rem] mr-[10rem]">
+          <img src="/logo.svg" alt="" className="h-10" />
+          <p className="mt-4 text-[1rem]">Welcome to OneBooking </p>
+          <h1 className="text-[2.2rem] font-semibold">Sign in</h1>
+          {/* Incorrect Username or Password */}
+          {incorrect && (
+            <div
+              className="flex w-[20rem] space-between self-center border-2 border-red-500 px-3 py-2 text-red-500 rounded-md mb-3"
+              onClick={() => setIncorrect(false)}
+            >
+              <small>Incorrect username or password. please try again!</small>
+              <ClearIcon className="cursor-pointer" />
+            </div>
+          )}
+          <div className="mt-4 flex flex-col gap-2">
+            <label htmlFor="username" className="text-xs">
+              Enter your username or email address
             </label>
-            <input type="text" className="input2 w-[18rem]" ref={username} />
+            <input
+              type="text"
+              placeholder="Username or email address"
+              id="username"
+              className="rounded-md focus:outline-none text-xs p-3"
+              {...register("username", { required: true })}
+            />
+            {errors.username && (
+              <span className="text-xs text-red-600">
+                Username or email is required
+              </span>
+            )}
           </div>
-          <div className="flex flex-col mt-2">
-            <label htmlFor="" className="font-semibold">
-              Password
+          <div className="mt-4 flex flex-col gap-2">
+            <label htmlFor="password" className="text-xs">
+              Enter your password
             </label>
-            <input type="text" className="input2 w-[18rem]" ref={password} />
+            <input
+              type="password"
+              placeholder="Password"
+              id="password"
+              className="rounded-md focus:outline-none text-xs p-3"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <span className="text-xs text-red-600">Password is required</span>
+            )}
+            <div className="text-xs flex justify-between">
+              <div className="flex gap-1 justify-center items-center">
+                <input type="checkbox" id="rememberMe" name="rememberMe" />
+                <label htmlFor="rememberMe">Remember me</label>
+              </div>
+              <div className="">Forget Password</div>
+            </div>
           </div>
-          <button
-            onClick={registeruser}
-            className="rounded-md h-[2.5rem] flex justify-center items-center font-semibold text-sm gap-3 bg-[#4cb050] w-[18rem] text-white mt-[1rem]"
-          >
-            Sign in
-          </button>
+          <div className="flex flex-row-reverse mt-[2rem] mb-[2rem]">
+            <button className="bg-[#4CAF50] rounded-md h-[2.3rem] flex justify-center items-center text-white text-sm w-[10rem]">
+              {isLoading ? (
+                <CircularProgress color="secondary" size="20px" />
+              ) : (
+                "Sign In"
+              )}
+            </button>
+          </div>
         </div>
       </div>
-      <div
-        className="flex-1 bg-[#fcfeff] w-full h-full"
-        style={{ backgroundImage: "url('/Visual.svg')" }}
-      ></div>
+      <div className="bg-white flex justify-center items-center p-3">
+        All Rights Reserved TCS - a TRANZUM enterprise
+      </div>
     </form>
   );
 };
